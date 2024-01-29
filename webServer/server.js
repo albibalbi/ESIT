a45c6d976bf1e6d7242c021b37436d3ed566f02c
HellNF <nicolofadiga@gmail.com>
2024-01-29 18:20:59 +0100

    added cons rules

--- webServer/server.js
+++ webServer/server.js
@@ -1,10 +1,14 @@
 const express= require('express')
 const bodyParser= require('body-parser')
+const cons = require('cons')
 
 const {insertTrk,findByDate,findAll}=require('./database')
 
 const app=express()
 const port = 3000
+app.use(cors({
+    origin: "http://13.60.21.191:4000/",
+}))
 app.use(bodyParser.json())
 
 app.post('/api',(req,res)=>{
